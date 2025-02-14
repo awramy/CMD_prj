9cc4b3e4e000f8b62248d033a90a351d0822c4c6
awramy <avramchech@gmail.com>
2025-02-14 12:10:22 +0300

    update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Убедитесь, что путь корректен
+  base: '/client', // Убедитесь, что путь корректен
   build: {
     outDir: 'dist', // Папка для сборки
   },
