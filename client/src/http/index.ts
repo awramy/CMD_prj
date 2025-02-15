727ee33beff18f505ed242c6727075b785793314
awramy <avramchech@gmail.com>
2025-02-15 21:17:49 +0300

    update

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -17,7 +17,7 @@ const authInterceptor = ( config: InternalAxiosRequestConfig ) => {
   const { query_id, user, hash } = window.Telegram.WebApp.initDataUnsafe
 
   config.headers.set({
-    user: user?.id || " ",
+    user_id: user?.id || " ",
     query_id: query_id || " ",
     hash: hash || " ",
   })
