cc4151f7fc265f5702791b9fb229ac1d3e8b540d
awramy <avramchech@gmail.com>
2025-02-16 10:13:17 +0300

    update

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 //получаем инфо о пользователе, возращаем найденного или созданного пользователя
 router.get('/user', authMW, userController.getOrCreateUser)
-router.get('/check', userController.checkUser)
+router.get('/check', authMW, userController.checkUser)
 
 
 export default router
