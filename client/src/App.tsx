800f17dce76965e4d0c6b984efbded5724f215a0
awramy <avramchech@gmail.com>
2025-02-16 12:52:00 +0300

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -64,7 +64,7 @@ const App: React.FC = () => {
       .then(data => console.log(data))
       .catch(err => console.log(err))
       .finally(() => setLoading(false))
-    , 2000)
+    , 1000)
     //заполняем контекст данными
     products.setProducts(productsArray)
     basket.setBasket((basketList))
